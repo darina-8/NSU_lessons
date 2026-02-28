2ee4742660514d5c14f99704724e32fbd01e88a4
darina-8 <darina.zelenckova@yandex.ru>
2026-02-28 11:14:03 +0700

    a

--- format.py
+++
@@ -1,8 +0,0 @@
-def format_number(n):
-    n = round(n, 3)
-    i = int(n)
-    frac = n - i
-    fstr = f"{frac:.3f}"[2:]
-    istr = f"{i:,}".replace(',', ' ')
-    res = f"{istr} .{fstr}"
-    return res.center(30, '*')
